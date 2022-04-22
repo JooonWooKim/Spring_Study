09b2f98f5a8a17dfe1bb8e90ea9155087487167a
Joonwoo Kim <kkn8010@gmail.com>
2022-04-22 16:37:24 +0900

    study: 조회 빈이 2개 이상일 경우 문제 해결(@Autowired 필드명, @Quilifier, @Primary 학습)

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,15 +3,25 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository,
+                            DiscountPolicy discountPolicy){
+                            //@Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy)
+                            //DiscountPolicy rateDiscountPolicy(필드명을 빈 이름으로 변경)
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+//        this.discountPolicy = rateDiscountPolicy;(필드명을 빈 이름으로 변경)
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
