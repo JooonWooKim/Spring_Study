865da3597005829faeb1fa9dec94d33025dbaef0
Joonwoo Kim <kkn8010@gmail.com>
2022-05-03 21:04:24 +0900

    docs: domain class 작성

---
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package com.jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //꼭 STRING 사용, 중간에 다른것이 끼면 장애발생
+    private DeliveryStatus status; //READY, COMP
+}
